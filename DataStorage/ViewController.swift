ded16577c77cbf58edde747234c240f7c4627203
Yuliia Khrupina <julichka1391@gmail.com>
2022-03-12 22:13:44 -0800

    Initial Commit

---
+++ DataStorage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataStorage
+//
+//  Created by Yuliia Khrupina on 3/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
