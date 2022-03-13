89f7beb3a107a1d8ba7dbdaf6b968fc6e2f837a1
Yuliia Khrupina <julichka1391@gmail.com>
2022-03-12 22:35:00 -0800

    userdafeults added

--- DataStorage/ViewController.swift
+++ DataStorage/ViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var switcher: UISwitch!
+    
+    let lightImageName: String = "light"
+    let darkImageName: String = "dark"
+    
+    let imageNameKey: String = "imageNameKey"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        UserDefaults.standard.set(lightImageName, forKey: imageNameKey)
+        image.image = UIImage(named: lightImageName)
     }
 
 
