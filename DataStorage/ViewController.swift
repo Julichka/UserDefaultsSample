333cbe56b4fbef4dc184efccee4dd459c41da956
Yuliia Khrupina <julichka1391@gmail.com>
2022-03-12 23:05:04 -0800

    ch logic

--- DataStorage/ViewController.swift
+++ DataStorage/ViewController.swift
@@ -12,24 +12,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var switcher: UISwitch!
     
-    let lightImageName: String = "light"
-    let darkImageName: String = "dark"
+    let image2: String = "light"
+    let image1: String = "dark"
     
     let lightBackColor = UIColor.white
     let darkBackColor = UIColor.black
     
     let imageNameKey: String = "imageNameKey"
-    let isBackgroundLightKey: String = "isBackgroundLightKey"
+    let isBackgroundDarkKey: String = "isBackgroundDarkKey"
     
     let defaults: UserDefaults = UserDefaults.standard
     
     @IBAction func onSwitched(_ sender: Any) {
         if switcher.isOn {
-            defaults.set(lightImageName, forKey: imageNameKey)
-            defaults.set(false, forKey: isBackgroundLightKey)
+            defaults.set(image2, forKey: imageNameKey)
+            defaults.set(true, forKey: isBackgroundDarkKey)
         } else {
-            defaults.set(darkImageName, forKey: imageNameKey)
-            defaults.set(true, forKey: isBackgroundLightKey)
+            defaults.set(image1, forKey: imageNameKey)
+            defaults.set(false, forKey: isBackgroundDarkKey)
         }
         updateUI()
     }
@@ -42,16 +42,16 @@ class ViewController: UIViewController {
     func updateUI() {
         var savedFileName = defaults.string(forKey: imageNameKey)
         if (savedFileName == nil) {
-            savedFileName = lightImageName
+            savedFileName = image1
             defaults.set(savedFileName, forKey: imageNameKey)
         }
         
-        image.image = UIImage(named: savedFileName ?? lightImageName)
+        image.image = UIImage(named: savedFileName ?? image1)
         
-        if (defaults.bool(forKey: isBackgroundLightKey)) {
-            self.view.backgroundColor = lightBackColor
-        } else {
+        if (defaults.bool(forKey: isBackgroundDarkKey)) {
             self.view.backgroundColor = darkBackColor
+        } else {
+            self.view.backgroundColor = lightBackColor
         }
     }
 }
